7bc7fe6e5e96ef9702ebaf0e26975754505b0496
Krzysztof Pieniążek <krzysztof.pieniazek@hltech.com>
2021-03-22 09:53:02 +0100

    Rename stream type to stream name

--- src/main/java/com/hltech/store/examples/aggregate/OrderRepository.java
+++ src/main/java/com/hltech/store/examples/aggregate/OrderRepository.java
@@ -11,14 +11,14 @@ class OrderRepository extends AggregateRepository<Order, Event> {
 
     private static final Supplier<Order> INITIAL_AGGREGATE_STATE_SUPPLIER = Order::new;
     private static final BiFunction<Order, Event, Order> AGGREGATE_EVENT_APPLIER = Order::apply;
-    private static final String STREAM_TYPE = "OrderStream";
+    private static final String STREAM_NAME = "OrderStream";
 
     public OrderRepository(EventStore<Event> eventStore) {
         super(
                 eventStore,
                 INITIAL_AGGREGATE_STATE_SUPPLIER,
                 AGGREGATE_EVENT_APPLIER,
-                STREAM_TYPE
+                STREAM_NAME
         );
     }
 
